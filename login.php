eaf832a893b74fbbf588f7795e8019c281aeb216
Aditia Dika <75602174+aditiadika256@users.noreply.github.com>
2021-05-03 22:31:59 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,97 +1,25 @@
-<?php 
-require('connection.php');
-session_start();
-if(isset($_POST['submit'])&&!empty($_POST['submit'])){
-    $usernama = $_POST['username'];
-    $hashpassword = md5($_POST['pwd']);
-    $sql ="SELECT * FROM mahasiswa WHERE username = '$username' AND password = $password";
-    $data = pg_query($conn,$sql); 
-    $login_check = pg_num_rows($data);
-    if($login_check > 0){ 
-        
-        $_SESSION['user']=$username;
-        echo "<script> location= 'home.php'; </script>";  
-    }else{
-        
-        echo "<script>
-        alert('Email atau password salah');
-        </script>";
-    }
-}
 
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+require 'connection.php';
 
-    <title>Login</title>
-  </head>
-  <body>
+if (isset($_POST["login"])){
 
-    <div class="container">
-  <h2>Login Here </h2>
-  <form method="post">
-  
-     
-    <div class="form-group">
-      <label for="username">Username:</label>
-      <input type="email" class="form-control" id="username" placeholder="Enter username" name="username">
-    </div>
-    
-     
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-    </div>
-     
-    <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+$username = $_POST["username"];
+$password = $_POST["password"];
 
-    <div class="form-group">
-    <a href="register.php">Click here to register</a>
-    </div>
-  </form>
-</div>
+  $result = pg_query($conn, "SELECT * FROM mahasiswa WHERE username = '$username' AND password = $password");
 
-    <!-- Optional JavaScript; choose one of the two! -->
+if (password_verify($password, $row["password"])){
+      set session
+      $_SESSION["login"] = true;
+      $_SESSION["username"] = $username;
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
-
-<!-- <?php
-
-//require 'connection.php';
-
-//if (isset($_POST["login"])){
-
-  //$username = $_POST["username"];
-  //$password = $_POST["password"];
-
-  $result //= pg_query($conn, "SELECT * FROM mahasiswa WHERE username = '$username' AND password = $password");
-
-  //if (password_verify($password, $row["password"])){
-      // set session
-      //$_SESSION["login"] = true;
-      //$_SESSION["username"] = $username;
-
-      //header("Location: indexnelayan.php");
-      //exit;
+      header("Location: indexnelayan.php");
+      exit;
     }
 
-  //header("location: about.php");
+  header("location: about.php");
 
 }
 ?>
@@ -154,4 +82,3 @@ if(isset($_POST['submit'])&&!empty($_POST['submit'])){
 </body>
 
 </html>
- -->
