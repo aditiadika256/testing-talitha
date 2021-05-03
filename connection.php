2d08b61c1220d6a98d64e7e5e3bb647ae33dd08b
Aditia Dika <75602174+aditiadika256@users.noreply.github.com>
2021-05-03 22:07:52 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,12 @@
 <?php
 
-$conn = pg_connect("ec2-23-23-128-222.compute-1.amazonaws.com
-port= 5432
-dbname= d1lv98h65q8rd5
-user= pboobdgwyaitgo
-password= 4da227f86f5b021a6a235d3a16319dd745cf48a0217efd108ec1ed650096bd0e");
+$host = "ec2-23-23-128-222.compute-1.amazonaws.com";
+$port = "5432";
+$dbname = "d1lv98h65q8rd5";
+$user = "pboobdgwyaitgo";
+$password = "4da227f86f5b021a6a235d3a16319dd745cf48a0217efd108ec1ed650096bd0e"; 
+$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
+$conn = pg_connect($connection_string);
 
 
 ?>
